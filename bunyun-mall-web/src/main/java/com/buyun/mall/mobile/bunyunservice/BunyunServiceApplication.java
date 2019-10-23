e438aa7423ad184baba7f32049f8d4e504bd336b
You-Hmily <706097141@qq.com>
2019-10-23 23:00:50 +0800

    初始化项目

---
+++ bunyun-mall-web/src/main/java/com/buyun/mall/mobile/bunyunservice/BunyunServiceApplication.java
@@ -0,0 +1,13 @@
+package com.buyun.mall.mobile.bunyunservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BunyunServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BunyunServiceApplication.class, args);
+	}
+
+}
---
+++ bunyun-mall-web/src/test/java/com/buyun/mall/mobile/bunyunservice/BunyunServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buyun.mall.mobile.bunyunservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunyunServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
